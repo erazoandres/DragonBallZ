20684a7716b5163885d456bfc31c9a09bdf5fd3f
Andrés Erazo <erazoandres14@gmail.com>
2024-06-18 00:34:20 -0500

    delirando v2

--- test.py
+++
@@ -1,9 +0,0 @@
-diccionario = {}
-diccionario["gohan"]   = "el que derroto a cell" 
-diccionario["vegeta"]  = "principe saiyan"
-diccionario["piccolo"] = "Padrastro de Gohan"
-
-descripcion = diccionario.values()
-llaves = diccionario.keys()
-
-print(type(descripcion))
